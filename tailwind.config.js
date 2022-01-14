33e40525c95c65210a93a317219eac4baf04237b
Odumz <odumz20@gmail.com>
2022-01-14 10:32:48 +0100

    utility plugin added in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 function withOpacity(variableName) {
   return ({ opacityValue }) => {
     if (opacityValue !== undefined) {
@@ -35,4 +37,18 @@ module.exports = {
       }
     },
   },
+  plugins: [
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        /* Hide scrollbar for Chrome, Safari and Opera */
+        '.no-scrollbar::-webkit-scrollbar': {
+          'display': 'none',
+        },
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',  /* Firefox */
+        }
+      })
+    })
+  ],
 }
